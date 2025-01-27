fcc6ba5875c818159d95c39ea9eae7840ec544ff
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2025-01-27 15:11:50 -0500

    Refactory ChakraUI version

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider";
 import "./index.css";
+import { ChakraProvider, Theme } from "@chakra-ui/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>
 );
